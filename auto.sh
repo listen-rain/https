65029540a094efe22e3129b63b0adc2415dcf2b6
listenwei <m15275049388@163.com>
2018-09-16 02:18:24 +0800

    pass

--- auto.sh
+++ auto.sh
@@ -5,7 +5,11 @@ cd $1
 echo "
 python ./acme_tiny.py --account-key ./account.key \
     --csr ./domain.csr \
-    --acme-dir "$challengeDir" > ./signed.crt || exit
+    --acme-dir "$challengeDir" > ./signed.crt > ./acme_tiny.log
+
+if [ $? != 0 ]; then
+    exit $?
+fi
 
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > ./intermediate.pem
 
@@ -16,5 +20,5 @@ nginx -s reload
 
 chmod a+x ./renew_cert.sh
 
-echo "exec: crontab -e '0 0 1 * * $workDir/renew_cert.sh 2>> $workDir/acme_tiny.log' "
+echo "Don't forget, exec: crontab -e '0 0 1 * * $workDir/renew_cert.sh 2>> $workDir/acme_tiny.log' "
 
