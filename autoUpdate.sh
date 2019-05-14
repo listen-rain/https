f1180a8b65a2aaa1de6ce4609577f7fa29e59dab
zhufengwei <zhufengwei@aliyun.com>
2019-05-14 14:50:19 +0800

    fix bug

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -3,8 +3,9 @@
 autoUpdate () {
     echo -e "\n\033[32m \bcreating autoUpdate script renew_cert.sh ...\033[0m"
 
-    if [ -f ./renew_cert.sh ]; then
-        read -p "./renew_cert.sh already exists, overwrite?  [yes|no]: " -a overwrite
+    renewFIle="renew_cert.sh"
+    if [ -f $renewFIle ]; then
+        read -p "$renewFIle already exists, overwrite?  [yes|no]: " -a overwrite
         if [ $overwrite == "no" ]; then
             echo -e "exiting ...\n"
             exit 0
@@ -16,8 +17,8 @@ autoUpdate () {
         --csr $1/domain.csr \
         --acme-dir "$2" > $1/signed.crt
 
-    if [ $? -gt 0 ]; then
-        exit $?
+    if [ \$? -gt 0 ]; then
+        exit \$?
     fi
 
     wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem > $1/intermediate.pem
@@ -25,9 +26,9 @@ autoUpdate () {
     cat $1/signed.crt $1/intermediate.pem > $1/chained.pem
 
     nginx -s reload
-    " | tee ./renew_cert.sh
+    " | tee ./$renewFIle
 
-    chmod a+x ./renew_cert.sh
-    echo -e "\033[33m \bDon't forget. exec: crontab -e '0 0 1 * * /usr/bin/sh $1/renew_cert.sh 2>> $1/acme_tiny.log' \033[0m"
+    chmod a+x ./$renewFIle
+    echo -e "\033[33m \bDon't forget. exec: crontab -e '0 0 1 * * /usr/bin/sh $1/$renewFIle 2>> $1/acme_tiny.log' \033[0m"
     exit 0
 }
